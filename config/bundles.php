8cdbad8d4e186a7d3b90b019ef6017e2de7c9d3b
tohidhabiby <tohidhabiby@users.noreply.github.com>
2025-10-06 23:23:49 +0000

    Revert to single stage Dockerfile and fix dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -1,17 +1,9 @@
 <?php
 
-$bundles = [
+return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
-
-if (class_exists(Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class)) {
-    $bundles[Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class] = ['dev' => true, 'stage' => true, 'test' => true];
-}
-
-if (class_exists(Nelmio\ApiDocBundle\NelmioApiDocBundle::class)) {
-    $bundles[Nelmio\ApiDocBundle\NelmioApiDocBundle::class] = ['dev' => true, 'stage' => true];
-}
-
-return $bundles;
