c2af826269e6e67b34b201b6bccf191e7f4c6224
tohidhabiby <tohidhabiby@users.noreply.github.com>
2025-10-06 22:02:34 +0000

    Fix production build by conditionally loading dev bundles

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,17 @@
 <?php
 
-return [
+$bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'stage' => true, 'test' => true],
-    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['dev' => true, 'stage' => true],
 ];
+
+if (class_exists(Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class)) {
+    $bundles[Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class] = ['dev' => true, 'stage' => true, 'test' => true];
+}
+
+if (class_exists(Nelmio\ApiDocBundle\NelmioApiDocBundle::class)) {
+    $bundles[Nelmio\ApiDocBundle\NelmioApiDocBundle::class] = ['dev' => true, 'stage' => true];
+}
+
+return $bundles;
