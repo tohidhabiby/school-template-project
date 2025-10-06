25cdb6f9d806926ad800e0ef88f248146a514a0b
tohidhabiby <tohidhabiby@users.noreply.github.com>
2025-10-06 21:38:18 +0000

    Add Swagger documentation to UserInfoController

--- src/Controller/UserInfoController.php
+++ src/Controller/UserInfoController.php
@@ -6,9 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+use OpenApi\Attributes as OA;
+
 class UserInfoController extends AbstractController
 {
     #[Route('/api/users', name: 'api_users')]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns the information of the users',
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(
+                type: 'object',
+                properties: [
+                    new OA\Property(property: 'id', type: 'integer'),
+                    new OA\Property(property: 'name', type: 'string'),
+                    new OA\Property(property: 'email', type: 'string'),
+                ]
+            )
+        )
+    )]
+    #[OA\Tag(name: 'Users')]
     public function index(): JsonResponse
     {
         return $this->json([]);
